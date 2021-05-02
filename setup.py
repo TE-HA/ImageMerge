fc63b11fa132f3a8d30c80594aa25af4b67cafe1
TE-HA <xys_whut_136@163.com>
2021-05-02 19:31:34 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mergepic",
+    version = "1.0",
+    keywords = ("image", "merge"),
+    description = "image merge",
+    long_description = "Provide a dictionary contains several images, row, every image scale (or padding), output merge result.",
+    license = "MIT Licence",
+
+    author = "xys",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy', 'opencv'],
+)
